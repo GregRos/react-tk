93246d03548744794518a766d80473e7c3b2b125
GregRos <life.gregr@gmail.com>
2025-09-27 21:36:07 +0300

    removed unneeded files

--- reactk/tk/root.py
+++
@@ -1,6 +0,0 @@
-from reactk.model.renderable.node.shadow_node import ShadowNode
-from reactk.tk.nodes.window import Window
-
-
-class TkRoot(ShadowNode[Window]):
-    pass
--- umm.py
+++
@@ -1,4 +0,0 @@
-from typing import Annotated, Any, TypeVar, Generic
-
-T = TypeVar("T")
-A = TypeVar("A", bound=Any)
